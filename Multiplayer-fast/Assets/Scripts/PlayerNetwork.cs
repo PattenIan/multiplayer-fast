2b4bf59367166b45525de3106c657c210311cf68
Noaskov <n.c.skov@gmail.com>
2023-04-03 15:02:36 +0200

    s

--- Multiplayer-fast/Assets/Scripts/PlayerNetwork.cs
+++ Multiplayer-fast/Assets/Scripts/PlayerNetwork.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
+using UnityEngine.Networking;
 
 
 public class PlayerNetwork : NetworkBehaviour
@@ -11,10 +12,10 @@ public class PlayerNetwork : NetworkBehaviour
     private void Awake()
     {
         cam = GetComponent<Camera>();
-        if (IsLocalPlayer)
-        {
+        if (IsLocalPlayer) return;
+        
             cam.enabled= false;
-        }
+        
         
     }
     private void Update()
